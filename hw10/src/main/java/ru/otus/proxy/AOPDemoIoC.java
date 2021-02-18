a3caf7fce1d83bd205361cb9275694c568044eb1
Жарницкий Давид Максимович <DZharnitskiy@alfabank.ru>
2021-02-18 13:34:16 +0300

    Add set for saving annotated methods

--- hw10/src/main/java/ru/otus/proxy/AOPDemoIoC.java
+++ hw10/src/main/java/ru/otus/proxy/AOPDemoIoC.java
@@ -4,16 +4,22 @@ import ru.otus.Log;
 
 import java.lang.reflect.*;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AOPDemoIoC {
 
     public static <T> T get(Class<T> clazz) {
         return (T) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), clazz.getInterfaces(), new InvocationHandler() {
             Object o;
+            Set<String> loggedMethods = new HashSet<>();
 
             {
                 try {
                     o = clazz.newInstance();
+                    Arrays.stream(clazz.getDeclaredMethods())
+                            .filter(method -> method.isAnnotationPresent(Log.class))
+                            .forEach(m -> loggedMethods.add(m.getName() + Arrays.toString(m.getParameterTypes())));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -21,7 +27,7 @@ public class AOPDemoIoC {
 
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                if (clazz.getDeclaredMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class))
+                if (loggedMethods.contains(method.getName() + Arrays.toString(method.getParameterTypes())))
                     System.out.println("My PROXY LOGGING for method: " + method.getName() + Arrays.asList(method.getParameterTypes()));
                 return method.invoke(o, args);
             }
